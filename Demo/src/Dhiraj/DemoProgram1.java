6adcf34bf47993ba0f1a06ed4d550e01704bb9e5
vinod <vinod@GQM-IT-LTP-043>
2019-03-12 13:19:37 +0530

    New

--- Demo/src/Dhiraj/DemoProgram1.java
+++ Demo/src/Dhiraj/DemoProgram1.java
@@ -4,7 +4,7 @@ public class DemoProgram1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Welcome to goquest...");
+		System.out.print("Hello");
 	}
 
 }
