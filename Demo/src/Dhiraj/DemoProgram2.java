485c9837a18709e379eb1b44d58e6aa722c6618d
dhiraj <dhiraj@GQM-LTP-IT-042>
2019-03-12 12:54:57 +0530

    Changes

---
+++ Demo/src/Dhiraj/DemoProgram2.java
@@ -0,0 +1,10 @@
+package Dhiraj;
+
+public class DemoProgram2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("hiiiiiiiiiiiiiiiiiiiiiii");
+	}
+
+}
