8954f67e914cf07a38916fa2aa5444f62fa4304a
dhiraj <dhiraj@GQM-LTP-IT-042>
2019-03-12 12:31:22 +0530

    Day-12/03/2019

---
+++ Demo/src/Dhiraj/DemoProgram1.java
@@ -0,0 +1,10 @@
+package Dhiraj;
+
+public class DemoProgram1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to goquest...");
+	}
+
+}
